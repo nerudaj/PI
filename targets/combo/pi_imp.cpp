3b73405b30d4d1ae46b6959535000325ef261e54
nerudaj <xnerud01@stud.fit.vutbr.cz>
2018-03-05 13:59:39 +0100

    Implement pi features
    * Assign device
    * Remove device
    * Table entry add

--- targets/combo/pi_imp.cpp
+++ targets/combo/pi_imp.cpp
@@ -20,20 +20,40 @@
 
 #include <PI/pi.h>
 #include <PI/target/pi_imp.h>
+#include <iostream>
 
 #include "api/P4Dev.hpp"
+#include "devices.hpp"
 
 extern "C" {
 
 pi_status_t _pi_init(void *extra) {
 	(void) extra;
+	std::cout << "PI_init\n";
 	return PI_STATUS_SUCCESS;
 }
 
 pi_status_t _pi_assign_device(pi_dev_id_t dev_id, const pi_p4info_t *p4info, pi_assign_extra_t *extra) {
-	(void)dev_id;
 	(void)p4info;
 	(void)extra;
+	std::cout << "PI_assign_device - " << dev_id << "\n";
+	
+	if (dev_id > DeviceManager::getDeviceCount()) {
+		return PI_STATUS_DEV_OUT_OF_RANGE;
+	}
+	else if (not DeviceManager::reserveDevice(dev_id)) {
+		return PI_STATUS_DEV_ALREADY_ASSIGNED;
+	}
+	
+	p4dev_name_t DEVICE_NAMES[] = { P4DEV_ID0, P4DEV_ID1 };
+	uint32_t status = devices[dev_id].initialize(DEVICE_NAMES[dev_id]);
+	if (status != P4DEV_OK) {
+		p4dev_err_stderr(status);
+		return pi_status_t(PI_STATUS_TARGET_ERROR + status);
+	}
+	
+	infos[dev_id] = p4info;
+	
 	return PI_STATUS_SUCCESS;
 }
 
@@ -42,42 +62,57 @@ pi_status_t _pi_update_device_start(pi_dev_id_t dev_id, const pi_p4info_t *p4inf
 	(void)p4info;
 	(void)device_data;
 	(void)device_data_size;
+	std::cout << "PI_update_device_start\n";
 	return PI_STATUS_SUCCESS;
 }
 
 pi_status_t _pi_update_device_end(pi_dev_id_t dev_id) {
 	(void)dev_id;
+	std::cout << "PI_update_device_start\n";
 	return PI_STATUS_SUCCESS;
 }
 
 pi_status_t _pi_remove_device(pi_dev_id_t dev_id) {
-	(void)dev_id;
+	std::cout << "PI_remove_device\n";
+	
+	if (dev_id > DeviceManager::getDeviceCount()) {
+		return PI_STATUS_DEV_OUT_OF_RANGE;
+	}
+	
+	devices[dev_id].deinitialize();
+	DeviceManager::freeDevice(dev_id);
+	
 	return PI_STATUS_SUCCESS;
 }
 
 pi_status_t _pi_destroy() {
+	std::cout << "PI_destroy\n";
 	return PI_STATUS_SUCCESS;
 }
 
-// bmv2 does not support transaction and has no use for the session_handle
+// Combo does not support transaction and has no use for the session_handle
 pi_status_t _pi_session_init(pi_session_handle_t *session_handle) {
 	(void)session_handle;
+	std::cout << "PI_session_init\n";
 	return PI_STATUS_SUCCESS;
 }
 
 pi_status_t _pi_session_cleanup(pi_session_handle_t session_handle) {
 	(void) session_handle;
+	std::cout << "PI_session_cleanup\n";
 	return PI_STATUS_SUCCESS;
 }
 
 pi_status_t _pi_batch_begin(pi_session_handle_t session_handle) {
 	(void) session_handle;
+	std::cout << "PI_batch_begin\n";
 	return PI_STATUS_SUCCESS;
 }
 
 pi_status_t _pi_batch_end(pi_session_handle_t session_handle, bool hw_sync) {
 	(void) session_handle;
 	(void) hw_sync;
+	std::cout << "PI_batch_end\n";
 	return PI_STATUS_SUCCESS;
 }
 
@@ -85,6 +120,7 @@ pi_status_t _pi_packetout_send(pi_dev_id_t dev_id, const char *pkt, size_t size)
 	(void)dev_id;
 	(void)pkt;
 	(void)size;
+	std::cout << "PI_packetout_send\n";
 	return PI_STATUS_SUCCESS;
 }
 
--- targets/combo/pi_tables_imp.cpp
+++ targets/combo/pi_tables_imp.cpp
@@ -23,16 +23,165 @@
 #include <PI/p4info.h>
 #include <PI/pi.h>
 
+#include <iostream>
+#include "devices.hpp"
+
+p4rule_t *createRule(const char *tableName, const pi_p4info_t *info, pi_p4_id_t table_id) {
+	// NOTE: Based on whether frontend or backend checks validity of incoming rules, this might get rewritten
+
+	std::size_t matchFieldsSize;
+	auto matchFields = pi_p4info_table_get_match_fields(info, table_id, &matchFieldsSize);
+	
+	// Determine engine of rule
+	p4engine_type_t engineType = P4ENGINE_TCAM;
+	bool lpmSet = true;
+	for (std::size_t i = 0; i < matchFieldsSize; i++) {
+		if (matchFields[i] != PI_P4INFO_MATCH_TYPE_LPM) lpmSet = false;
+	}
+	if (lpmSet) engineType = P4ENGINE_LPM;
+
+	p4rule_t *result = p4rule_create(tableName, engineType);
+	return result;
+}
+
+uint32_t addKeys(const pi_p4info_t *info, pi_p4_id_t table_id, const pi_match_key_t *match_key, p4rule_t *rule) {
+	const char *data = match_key->data;
+
+	p4key_elem_t *key;
+	uint8_t *value;
+	uint8_t *mask;
+	uint32_t status;
+
+	size_t matchFieldsSize = pi_p4info_table_num_match_fields(info, table_id);
+	for (std::size_t i = 0; i < matchFieldsSize; i++) {
+		const pi_p4info_match_field_info_t *fieldInfo = pi_p4info_table_match_field_info(info, table_id, i);
+		size_t bitwidth = fieldInfo->bitwidth;
+		size_t bytewidth = (bitwidth + 7) / 8;
+		uint32_t prefixLen;
+		const char *keyName = pi_p4info_table_match_field_name_from_id(info, table_id, fieldInfo->mf_id);
+
+		switch (fieldInfo->match_type) {
+		case PI_P4INFO_MATCH_TYPE_VALID:
+			return P4DEV_NOT_IMPLEMENTED;
+			break;
+
+		case PI_P4INFO_MATCH_TYPE_EXACT:
+			return P4DEV_NOT_IMPLEMENTED;
+			break;
+
+		case PI_P4INFO_MATCH_TYPE_LPM:
+			value = new uint8_t[bytewidth];
+			if (value == NULL) return P4DEV_ALLOCATE_ERROR;
+			memcpy(value, data, bytewidth);
+			data += bytewidth;
+			data += retrieve_uint32(data, &prefixLen);
+			key = bstlpm_p4key_create(keyName, bytewidth, value, prefixLen);
+			if ((status = p4rule_add_key_element(rule, key)) != P4DEV_OK) return P4DEV_ALLOCATE_ERROR;
+			break;
+
+		case PI_P4INFO_MATCH_TYPE_TERNARY:
+			value = new uint8_t[bytewidth];
+			mask = new uint8_t[bytewidth];
+
+			if (value == NULL or mask == NULL) return P4DEV_ALLOCATE_ERROR;
+
+			memcpy(value, data, bytewidth);
+			data += bytewidth;
+			memcpy(mask, data, bytewidth);
+			data += bytewidth;
+			key = tcam_p4key_create(keyName, bytewidth, value, mask);
+			if ((status = p4rule_add_key_element(rule, key)) != P4DEV_OK) return P4DEV_ALLOCATE_ERROR;
+
+			//*requires_priority = true;
+			break;
+
+		case PI_P4INFO_MATCH_TYPE_RANGE:
+			return P4DEV_NOT_IMPLEMENTED;
+			break;
+
+		default:
+			assert(0);
+			break;
+		}
+	}
+
+	return P4DEV_OK;
+}
+
+uint32_t addAction(const pi_p4info_t *info, const pi_action_data_t *action_data, p4rule_t *rule) {
+	assert(info);
+	assert(action_data);
+
+	pi_p4_id_t actionID = action_data->action_id;
+	const char *actionData = action_data->data;
+	const char *actionName = pi_p4info_action_name_from_id(info, actionID);
+
+	uint32_t status;
+	if ((status = p4rule_add_action(rule, actionName)) != P4DEV_OK) return status;
+
+	size_t paramIdsSize;
+	const pi_p4_id_t *paramIds = pi_p4info_action_get_params(info, actionID, &paramIdsSize);
+	for (size_t i = 0; i < paramIdsSize; i++) {
+		size_t paramBitwidth = pi_p4info_action_param_bitwidth(info, actionID, paramIds[i]);
+		size_t paramBytewidth = (paramBitwidth + 7) / 8;
+
+		uint8_t *data = new uint8_t[paramBytewidth];
+		if (data == NULL) return P4DEV_ALLOCATE_ERROR;
+
+		memcpy(data, actionData, paramBytewidth);
+		const char *paramName = pi_p4info_action_param_name_from_id(info, actionID, paramIds[i]);
+
+		p4param_t *param = p4param_create(paramName, paramBytewidth, data); // No need to test it, next call catches the error
+		if ((status = p4rule_add_param(rule, param)) != P4DEV_OK) return status;
+
+		actionData += paramBytewidth;
+	}
+
+	return P4DEV_OK;
+}
+
 extern "C" {
 
 pi_status_t _pi_table_entry_add(pi_session_handle_t session_handle, pi_dev_tgt_t dev_tgt, pi_p4_id_t table_id, const pi_match_key_t *match_key, const pi_table_entry_t *table_entry, int overwrite, pi_entry_handle_t *entry_handle) {
-	(void) overwrite;
-	(void) session_handle;
-	(void) dev_tgt;
-	(void) table_id;
-	(void) match_key;
-	(void) table_entry;
-	(void) entry_handle;
+	(void) overwrite; ///< We cannot determine if the rule is overwritten, yet
+	(void) session_handle; ///< No support for sessions
+	
+	const pi_p4info_t *info = infos[dev_tgt.dev_id];
+	assert(info != NULL);
+
+	// Retrieve table handle
+	const char *tableName = pi_p4info_table_name_from_id(info, table_id);
+	p4::TablePtr table = devices[dev_tgt.dev_id].getTable(tableName);
+	if (table == NULL) {
+		std::cerr << "Cannot get table with name: " << tableName << "\n";
+		return PI_STATUS_NETV_INVALID_OBJ_ID;
+	}
+	
+	// Initialize rule object
+	p4rule_t *rule = createRule(tableName, info, table_id);
+	if (rule == NULL) {
+		std::cerr << "Cannot create rule\n";
+		return pi_status_t(PI_STATUS_TARGET_ERROR);
+	}
+	uint32_t status;
+	if ((status = addKeys(info, table_id, match_key, rule)) != P4DEV_OK) {
+		p4dev_err_stderr(status);
+		return pi_status_t(PI_STATUS_TARGET_ERROR + status);
+	}
+	if ((status = addAction(info, table_entry->entry.action_data, rule)) != P4DEV_OK) {
+		p4dev_err_stderr(status);
+		return pi_status_t(PI_STATUS_TARGET_ERROR + status);
+	}
+	
+	// Insert rule to table
+	uint32_t ruleIndex;
+	if ((status = table->insertRule(rule, ruleIndex)) != P4DEV_OK) {
+		p4dev_err_stderr(status);
+		return pi_status_t(PI_STATUS_TARGET_ERROR + status);
+	}
+
+	*entry_handle = ruleIndex;
+	
 	return PI_STATUS_SUCCESS;
 }
 
