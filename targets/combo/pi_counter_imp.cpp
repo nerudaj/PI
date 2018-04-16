f8afeeb7f07cede045741f2144153bb0ca8779c7
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2018-04-16 10:45:07 +0200

    Runtime - Add basic support for registers (counters)

--- targets/combo/pi_counter_imp.cpp
+++ targets/combo/pi_counter_imp.cpp
@@ -22,24 +22,90 @@
 #include <PI/pi.h>
 #include <PI/target/pi_counter_imp.h>
 
+#include "devices.hpp"
+#include "helpers.hpp"
+
+bool isThisBigEndianSystem() {
+	uint32_t var = 1;
+	return ((uint8_t*)(&var))[0] == 1;
+}
+
+void flipEndianness(uint64_t &num) {
+	(uint8_t*) data = (&num);
+	for (int i = 0; i < sizeof(uint64_t) / 2; i++) {
+		std::swap(data[i], data[sizeof(uint64_t) - i - 1]);
+	}
+}
+
 extern "C" {
+
+/*
+NOTE: This could come in handy later (as defined in  PI/include/PI/pi_counter.h):
+#define PI_COUNTER_FLAGS_NONE 0
+// do a sync with the hw when reading a counter
+#define PI_COUNTER_FLAGS_HW_SYNC (1 << 0)
+*/
 
 pi_status_t _pi_counter_read(pi_session_handle_t session_handle, pi_dev_tgt_t dev_tgt, pi_p4_id_t counter_id, size_t index, int flags, pi_counter_data_t *counter_data) {
 	(void)session_handle;
-	(void)dev_tgt;
-	(void)counter_id;
-	(void)index;
 	(void)flags;
-	(void)counter_data;
+	
+	const pi_p4info_t *info = infos[dev_tgt.dev_id];
+	assert(info != NULL)
+	
+	const char *registerName = pi_p4info_counter_name_from_id(info, counter_id);
+	p4::RegisterPtr reg = devices[dev_tgt.dev_id].getRegister(registerName);
+	if (reg == NULL) {
+		std::cerr << "Cannot get register with name: " << registerName << "\n";
+		return PI_STATUS_NETV_INVALID_OBJ_ID;
+	}
+	
+	/*
+	NOTE: Combo card so far supports only registers. To use them, we're pretending that those registers are in fact
+	counters, working in BOTH mode, but the value of packets and bytes is equal to value of the register.
+	Data read from the register are stored in LSB, so MSB based machines should invert them.
+	*/
+	uint8_t data[sizeof(uint64_t)] = {0};
+	uint32_t status = reg->read(data, sizeof(uint64_t), index);
+	if (status != P4DEV_OK) {
+		p4dev_err_stderr(status);
+		return pi_status_t(PI_STATUS_TARGET_ERROR + status);
+	}
+	
+	counter_data->valid = PI_COUNTER_UNIT_PACKETS | PI_COUNTER_UNIT_BYTES;
+	counter_data->bytes = &((uint64_t*)(data));
+	counter_data->packets = &((uint64_t*)(data));
+	
+	if (isThisBigEndianSystem()) {
+		flipEndianness(counter_data->bytes);
+		flipEndianness(counter_data->packets);
+	}
+	
 	return PI_STATUS_SUCCESS;
 }
 
 pi_status_t _pi_counter_write(pi_session_handle_t session_handle, pi_dev_tgt_t dev_tgt, pi_p4_id_t counter_id, size_t index, const pi_counter_data_t *counter_data) {
 	(void)session_handle;
-	(void)dev_tgt;
-	(void)counter_id;
-	(void)index;
-	(void)counter_data;
+	
+	const pi_p4info_t *info = infos[dev_tgt.dev_id];
+	assert(info != NULL)
+	
+	const char *registerName = pi_p4info_counter_name_from_id(info, counter_id);
+	p4::RegisterPtr reg = devices[dev_tgt.dev_id].getRegister(registerName);
+	if (reg == NULL) {
+		std::cerr << "Cannot get register with name: " << registerName << "\n";
+		return PI_STATUS_NETV_INVALID_OBJ_ID;
+	}
+	
+	/*
+	See NOTE in the previous method to see why I am doing what I am doing.
+	*/
+	uint32_t status = reg->write((uint8_t *)(&counter_data->bytes), sizeof(uint64_t), index);
+	if (status != P4DEV_OK) {
+		p4dev_err_stderr(status);
+		return pi_status_t(PI_STATUS_TARGET_ERROR + status);
+	}
+	
 	return PI_STATUS_SUCCESS;
 }
 
