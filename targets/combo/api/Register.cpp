83fa1795e32f6d5ae0fcad3033d2024c4683cc74
Jakub Neruda <xnerud01@stud.fit.vutbr.cz>
2018-04-16 12:27:40 +0200

    Register - Fix typo

--- targets/combo/api/Register.cpp
+++ targets/combo/api/Register.cpp
@@ -1,7 +1,7 @@
 #include "P4Dev.hpp"
 
 uint32_t p4::Register::read(uint8_t* read_data, const uint32_t length, uint32_t reg_ind) {
-	return p4dev_register_read(deviceInfo, regInfo, read_write, length, reg_ind);
+	return p4dev_register_read(deviceInfo, regInfo, read_data, length, reg_ind);
 }
 
 uint32_t p4::Register::write(uint8_t* data_to_write, const uint32_t length, uint32_t reg_ind) {
